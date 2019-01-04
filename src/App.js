767e85afdb1d673494ec589b8ad7e6484a95615b
BradHick <bradleyhrm@gmail.com>
2019-01-03 23:23:18 -0300

    start configs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { NavLink, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import ContactListPage from './views/contact-list-page';
+import ContactFormPage from './views/contact-form-page';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Container>
+          <div className='ui two item menu'>
+            <NavLink className="item" 
+              activeClassName="active" 
+              exact to="/">Contacts List
+            </NavLink>
+            <NavLink className="item" 
+              activeClassName="active" 
+              exact to="/contacts/new">Add Contact
+            </NavLink>
+          </div>
+
+            <Route exact path="/" component={ContactListPage}/>
+            <Route path="/contacts/new" component={ContactFormPage}/>
+            <Route path="/contacts/edit/:_id" component={ContactFormPage}/>
+        </Container>
       </div>
     );
   }
