ee5f99c1b687ee9d4bb8240e1e1420f8be153ff2
BradHick <bradleyhrm@gmail.com>
2019-01-05 14:38:25 -0300

    refactor(contact-list): show message if contacts was empty

--- src/components/contact-list.jsx
+++ src/components/contact-list.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import { Card } from 'semantic-ui-react';
-import ContactCard from './contact-card';
+import ContactCard from './Contact-card';
 
 export default function ContactList({contacts, deleteContact}){
 
   const cards = () => {
+    if (contacts.length <= 1){
+      return (
+        <h3>Nenhum contato</h3>
+      );
+    }
     return contacts.map(contact => {
       return (
         <ContactCard key={contact._id} 
@@ -15,6 +20,7 @@ export default function ContactList({contacts, deleteContact}){
   };
   return (
     <Card.Group>
+      {console.log('contacts -> ', contacts)}
       { cards() }
     </Card.Group>
   );
