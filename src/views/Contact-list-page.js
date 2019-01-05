e15bf8107bd8789eefe5856f9b4f636b5ed1e6a6
BradHick <bradleyhrm@gmail.com>
2019-01-05 18:52:39 -0300

    replaced with a coprrect name

---
+++ src/views/Contact-list-page.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+
+import ContactList from '../components/Contact-list';
+
+
+class ContactListPage extends Component {
+  componentDidMount() {
+    this.props.fetchContacts();
+  };
+  
+  render(){
+    return (
+      <div>
+        {console.log('list-page this.props ->',this.props)}
+        <h1>List of contacts</h1>
+        <ContactList 
+          contacts={this.props.contacts} 
+          deleteContact={this.props.deleteContact} 
+        />
+      </div>
+    );
+  };
+};
+
+
+export default ContactListPage;
