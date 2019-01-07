cd38672a4388c0fc04209167a71e2f275819cc96
BradHick <bradleyhrm@gmail.com>
2019-01-07 16:31:52 -0300

    feat(app): create context provider

---
+++ src/container/Provider.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import { client } from '../client/client';
+
+const DEFAULT_STATE = {
+  contacts: [],
+  contact: {name:{}},
+  loading: false,
+  errors: {},
+  redirect: false
+};
+
+const url = '/contact';
+
+export const ContactContext = React.createContext();
+
+export default class Provider extends React.Component {
+  
+  state = DEFAULT_STATE;
+
+  cancelForm = () =>{
+    return this.setState({...this.state, redirect: true});
+  };
+
+  newContact = () =>{
+    return this.setState({...this.state, contact: {name:{}}, redirect: false});
+  };
+
+  saveContact = (contact) =>{
+    return client.post(url, contact)
+      .then(res =>{
+        console.log('------------------------------------');
+        console.log('saveContact res ->', res);
+        console.log('------------------------------------');
+        this.setState({...this.state, redirect: true});
+      });
+  };
+
+  fetchContact = (_id) =>{
+    return client.get(`${url}/${_id}`)
+      .then(res => {
+        console.log('------------------------------------');
+        console.log('fetchContact res ->', res);
+        console.log('------------------------------------');
+        return this.setState({...this.state, contact: res.data, redirect: false});
+      });
+  };
+
+
+  fetchContacts = () => {
+    return client.get(url).then(res => {
+      console.log('------------------------------------');
+      console.log('fetchContacts res ->', res);
+      console.log('------------------------------------');
+      return this.setState({...this.state, contacts: res.data.data, redirect: false});
+    });
+  }
+
+  deleteContact = (_id) => {
+    return client.delete(`${url}/${_id}`)
+      .then(res => {
+        console.log('------------------------------------');
+        console.log('deleteContact res ->', res);
+        console.log('------------------------------------');
+        this.fetchContacts();
+      });
+  };
+
+  updateContact = (contact) =>{
+    return client.put(`${url}/${contact._id}`, contact)
+      .then(res =>{
+        console.log('------------------------------------');
+        console.log('updateContact res ->', res);
+        console.log('------------------------------------');
+        this.setState({...this.state, redirect: true});
+      });
+  };
+
+  render(){
+    return (
+      <ContactContext.Provider
+        value={{
+          contacts: this.state.contacts,
+          contact: this.state.contact,
+          redirect: this.state.redirect,
+          saveContact: this.saveContact,
+          fetchContacts: this.fetchContacts,
+          fetchContact: this.fetchContact,
+          deleteContact: this.deleteContact,
+          updateContact: this.updateContact,
+          newContact: this.newContact,
+          cancelForm: this.cancelForm
+        }}
+      >
+        {this.props.children}
+      </ContactContext.Provider>
+    );
+  };
+
+};
