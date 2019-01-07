337380ff58a90403d1bcaa800a965c412ac08265
BradHick <bradleyhrm@gmail.com>
2019-01-07 16:32:48 -0300

    refactor(app): apply context components

--- src/App.js
+++ src/App.js
@@ -4,85 +4,87 @@ import { Container } from 'semantic-ui-react';
 import ContactListPage from './views/Contact-list-page';
 import ContactFormPage from './views/Contact-form-page';
 import { client } from './client/client';
+import { ContactContext } from './container/Provider';
 import './App.css';
 
-const url = '/contact';
+// const url = '/contact';
 
 
-const ContactContext = createContext();
+// const ContactContext = createContext();
 
 class App extends Component {
 
 
-  state ={
-    contacts: [],
-    contact: {name:{}},
-    loading: false,
-    errors: {},
-    redirect: false
-  };
+  // state ={
+  //   contacts: [],
+  //   contact: {name:{}},
+  //   loading: false,
+  //   errors: {},
+  //   redirect: false
+  // };
 
-  cancelForm = () =>{
-    return this.setState({...this.state, redirect: true});
-  };
+  // cancelForm = () =>{
+  //   return this.setState({...this.state, redirect: true});
+  // };
 
-  newContact = () =>{
-    return this.setState({...this.state, contact: {name:{}}, redirect: false});
-  };
+  // newContact = () =>{
+  //   return this.setState({...this.state, contact: {name:{}}, redirect: false});
+  // };
 
-  saveContact = (contact) =>{
-    return client.post(url, contact)
-      .then(res =>{
-        console.log('------------------------------------');
-        console.log('saveContact res ->', res);
-        console.log('------------------------------------');
-        this.setState({...this.state, redirect: true});
-      });
-  };
+  // saveContact = (contact) =>{
+  //   return client.post(url, contact)
+  //     .then(res =>{
+  //       console.log('------------------------------------');
+  //       console.log('saveContact res ->', res);
+  //       console.log('------------------------------------');
+  //       this.setState({...this.state, redirect: true});
+  //     });
+  // };
 
-  fetchContact = (_id) =>{
-    return client.get(`${url}/${_id}`)
-      .then(res => {
-        console.log('------------------------------------');
-        console.log('fetchContact res ->', res);
-        console.log('------------------------------------');
-        return this.setState({...this.state, contact: res.data, redirect: false});
-      });
-  };
+  // fetchContact = (_id) =>{
+  //   return client.get(`${url}/${_id}`)
+  //     .then(res => {
+  //       console.log('------------------------------------');
+  //       console.log('fetchContact res ->', res);
+  //       console.log('------------------------------------');
+  //       return this.setState({...this.state, contact: res.data, redirect: false});
+  //     });
+  // };
 
 
-  fetchContacts = () => {
-    return client.get(url).then(res => {
-      console.log('------------------------------------');
-      console.log('fetchContacts res ->', res);
-      console.log('------------------------------------');
-      return this.setState({...this.state, contacts: res.data.data, redirect: false});
-    });
-  }
+  // fetchContacts = () => {
+  //   return client.get(url).then(res => {
+  //     console.log('------------------------------------');
+  //     console.log('fetchContacts res ->', res);
+  //     console.log('------------------------------------');
+  //     return this.setState({...this.state, contacts: res.data.data, redirect: false});
+  //   });
+  // }
 
-  deleteContact = (_id) => {
-    return client.delete(`${url}/${_id}`)
-      .then(res => {
-        console.log('------------------------------------');
-        console.log('deleteContact res ->', res);
-        console.log('------------------------------------');
-        this.fetchContacts();
-      });
-  };
+  // deleteContact = (_id) => {
+  //   return client.delete(`${url}/${_id}`)
+  //     .then(res => {
+  //       console.log('------------------------------------');
+  //       console.log('deleteContact res ->', res);
+  //       console.log('------------------------------------');
+  //       this.fetchContacts();
+  //     });
+  // };
 
-  updateContact = (contact) =>{
-    return client.put(`${url}/${contact._id}`, contact)
-      .then(res =>{
-        console.log('------------------------------------');
-        console.log('updateContact res ->', res);
-        console.log('------------------------------------');
-        this.setState({...this.state, redirect: true});
-      });
-  };
+  // updateContact = (contact) =>{
+  //   return client.put(`${url}/${contact._id}`, contact)
+  //     .then(res =>{
+  //       console.log('------------------------------------');
+  //       console.log('updateContact res ->', res);
+  //       console.log('------------------------------------');
+  //       this.setState({...this.state, redirect: true});
+  //     });
+  // };
 
   render() {
     return (
       <div className="App">
+        {console.log('this ->', this)}
         <Container>
           <div className='ui pointing secondary menu'>
             <NavLink className="item" 
@@ -98,30 +100,14 @@ class App extends Component {
           <div className='twelve wide stretched column'>
           <div className="ui segment">
 
-            <ContactContext.Provider 
-              value={{
-                contacts: this.state.contacts,
-                contact: this.state.contact,
-                redirect: this.state.redirect,
-                saveContact: this.saveContact,
-                fetchContacts: this.fetchContacts,
-                fetchContact: this.fetchContact,
-                deleteContact: this.deleteContact,
-                updateContact: this.updateContact,
-                newContact: this.newContact,
-                cancelForm: this.cancelForm
-              }}
-            >
-              <ContactContext.Consumer>
-                { ({ fetchContact, contacts, deleteContact, fetchContacts, contact, redirect, cancelForm, saveContact, updateContact, newContact}) => (
                   <Fragment>
                     <Route 
                       exact path="/"
                       render={
                         () => <ContactListPage
-                          contacts={contacts}
-                          deleteContact={deleteContact}
-                          fetchContacts={fetchContacts}
+                          contacts={this.props.contacts}
+                          deleteContact={this.props.deleteContact}
+                          fetchContacts={this.props.fetchContacts}
                         />
                       }
                     />
@@ -130,12 +116,12 @@ class App extends Component {
                       path="/contacts/new"
                       render={
                         () => <ContactFormPage
-                          contact={contact}
-                          redirect={redirect}
-                          cancelForm={cancelForm}
-                          saveContact={saveContact}
-                          updateContact={updateContact}
-                          newContact={newContact}
+                          contact={this.props.contact}
+                          redirect={this.props.redirect}
+                          cancelForm={this.props.cancelForm}
+                          saveContact={this.props.saveContact}
+                          updateContact={this.props.updateContact}
+                          newContact={this.props.newContact}
                         />
                       }
                     />
@@ -144,21 +130,18 @@ class App extends Component {
                       path="/contacts/edit/:_id"
                       render={
                         () => <ContactFormPage
-                          contact={contact}
-                          redirect={redirect}
-                          cancelForm={cancelForm}
-                          saveContact={saveContact}
-                          updateContact={updateContact}
-                          newContact={newContact}
-                          fetchContact={fetchContact}
+                          contact={this.props.contact}
+                          redirect={this.props.redirect}
+                          cancelForm={this.props.cancelForm}
+                          saveContact={this.props.saveContact}
+                          updateContact={this.props.updateContact}
+                          newContact={this.props.newContact}
+                          fetchContact={this.props.fetchContact}
                         />
                       }
                     />
                   </Fragment>
-
-                )}
-              </ContactContext.Consumer>
-            </ContactContext.Provider>
+            
           </div>
           </div>
           
